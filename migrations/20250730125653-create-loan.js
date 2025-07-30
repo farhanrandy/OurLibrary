98adb495228339c6c172599834a62ab9ce54fd1e
Anggah123 <anggadinarga@gmail.com>
2025-07-30 21:01:13 +0700

    sampe seed user

---
+++ migrations/20250730125653-create-loan.js
@@ -0,0 +1,49 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Loans', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references:{
+          model: 'Users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      BookId: {
+        type: Sequelize.INTEGER,
+        references:{
+          model: 'Books',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      borrowDate: {
+        type: Sequelize.DATE
+      },
+      returnDate: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Loans');
+  }
+};
